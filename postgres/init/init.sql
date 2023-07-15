3bebb0c434829794d3dbd5e121f42b2467bc931c
gcchaan <gcchaan@users.noreply.github.com>
2023-07-15 13:38:00 +0900

    finish tutorial

--- postgres/init/init.sql
+++ postgres/init/init.sql
@@ -1,15 +1,37 @@
--- DB作成
-CREATE DATABASE sample_db;
--- 作成したDBに接続
-\c sample_db;
--- テーブル作成
-DROP TABLE IF EXISTS sample;
-CREATE TABLE sample (
-	id integer NOT NULL PRIMARY KEY,
-	name char(100) NOT NULL,
-	created_date_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+create database raw;
+\c raw;
+
+create schema if not exists jaffle_shop;
+create schema if not exists stripe;
+
+drop table if exists jaffle_shop.customers;
+create table jaffle_shop.customers(
+    id integer,
+    first_name varchar(50),
+    last_name varchar(50)
 );
--- ID用シーケンス作成
-CREATE SEQUENCE sample_id_seq START 1;
--- サンブルデータの登録
-INSERT INTO sample (id, name) VALUES(nextval('sample_id_seq'), 'sample name');
+
+drop table if exists jaffle_shop.orders;
+create table jaffle_shop.orders(
+    id integer,
+    user_id integer,
+    order_date date,
+    status varchar(50),
+    _etl_loaded_at timestamp default current_timestamp
+);
+
+drop table if exists stripe.payment;
+create table stripe.payment(
+    id integer,
+    orderid integer,
+    paymentmethod varchar(50),
+    status varchar(50),
+    amount integer,
+    created date,
+    _batched_at timestamp default current_timestamp
+);
+
+\copy jaffle_shop.customers from /csv/jaffle_shop_customers.csv with csv header
+-- _etl_loaded_at にデフォルトを扱わせるために列を指定する
+\copy jaffle_shop.orders (id, user_id, order_date, status) from /csv/jaffle_shop_orders.csv with csv header
+\copy stripe.payment (id, orderid, paymentmethod, status, amount, created) from /csv/stripe_payments.csv with csv header
